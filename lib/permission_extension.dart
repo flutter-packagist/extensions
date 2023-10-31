1ee02e83ea63d541b702d3745c337e2252ae7420
LioLin <windows7lake@gmail.com>
2023-10-31 14:58:10 +0800

    feat: 依赖更新

--- lib/permission_extension.dart
+++ lib/permission_extension.dart
@@ -2,7 +2,6 @@ import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:route/route.dart';
 
 /// 权限扩展：
 /// 1. 申请权限被永久拒绝后统一处理
@@ -19,78 +18,9 @@ extension PermissionActionHandle on Permission {
     } else if (onDenied != null) {
       onDenied.call(status);
     } else if (status.isPermanentlyDenied) {
-      settingDialog();
+      PermissionContext.showSettingDialog();
     }
     return status;
-  }
-
-  Future settingDialog({String? description}) {
-    return Go.dialog(Dialog(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const SizedBox(height: 20),
-          const Text(
-            '提示',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(height: 12),
-          Text(
-            description ?? '請在設置中打開權限',
-            style: const TextStyle(
-              color: Colors.black87,
-              fontSize: 16,
-            ),
-          ),
-          const SizedBox(height: 20),
-          Container(height: 0.5, width: 300, color: Colors.black12),
-          SizedBox(
-            width: 300,
-            height: 50,
-            child: Row(children: [
-              Expanded(
-                child: TextButton(
-                  onPressed: () => Go.back(),
-                  style: ButtonStyle(
-                    overlayColor: MaterialStateProperty.all(Colors.transparent),
-                  ),
-                  child: const Text(
-                    '取消',
-                    style: TextStyle(
-                      color: Colors.black54,
-                      fontSize: 16,
-                    ),
-                  ),
-                ),
-              ),
-              Container(height: 50, width: 0.5, color: Colors.black12),
-              Expanded(
-                child: TextButton(
-                  onPressed: () => openAppSettings(),
-                  style: ButtonStyle(
-                    overlayColor: MaterialStateProperty.all(Colors.transparent),
-                  ),
-                  child: const Text(
-                    '前往',
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontSize: 16,
-                    ),
-                  ),
-                ),
-              ),
-            ]),
-          ),
-        ],
-      ),
-    ));
   }
 
   // 在 Android 上，Permission.storage 权限链接到 Android READ_EXTERNAL_STORAGE 和 WRITE_EXTERNAL_STORAGE 权限。
@@ -113,5 +43,102 @@ extension PermissionActionHandle on Permission {
       }
     }
     return permission;
+  }
+}
+
+class PermissionContext {
+  static BuildContext? _context;
+  static String? _title;
+  static String? _description;
+  static String? _cancelText;
+  static String? _confirmText;
+
+  static void initContext(
+    BuildContext context, {
+    String? title,
+    String? description,
+    String? cancelText,
+    String? confirmText,
+  }) {
+    _context = context;
+    _title = title;
+    _description = description;
+    _cancelText = cancelText;
+    _confirmText = confirmText;
+  }
+
+  static Future showSettingDialog() {
+    assert(_context != null, 'Please call initContext() method first.');
+    return showDialog(
+      context: _context!,
+      builder: (context) => Dialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const SizedBox(height: 20),
+            Text(
+              _title ?? '提示',
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 12),
+            Text(
+              _description ?? '请在设置中打开权限',
+              style: const TextStyle(
+                color: Colors.black87,
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(height: 0.5, width: 300, color: Colors.black12),
+            SizedBox(
+              width: 300,
+              height: 50,
+              child: Row(children: [
+                Expanded(
+                  child: TextButton(
+                    onPressed: () => Navigator.of(_context!).pop(),
+                    style: ButtonStyle(
+                      overlayColor:
+                          MaterialStateProperty.all(Colors.transparent),
+                    ),
+                    child: Text(
+                      _cancelText ?? '取消',
+                      style: const TextStyle(
+                        color: Colors.black54,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(height: 50, width: 0.5, color: Colors.black12),
+                Expanded(
+                  child: TextButton(
+                    onPressed: () => openAppSettings(),
+                    style: ButtonStyle(
+                      overlayColor:
+                          MaterialStateProperty.all(Colors.transparent),
+                    ),
+                    child: Text(
+                      _confirmText ?? '前往',
+                      style: const TextStyle(
+                        color: Colors.blue,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              ]),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
